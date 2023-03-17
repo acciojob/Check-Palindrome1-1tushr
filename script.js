f2d8202a877dbe88b8043d43374978eaaa4bc1c2
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-03-17 20:15:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
-
+let str1=str;
+for(let i=str.length;i>=0;i--)
+	{
+		if(str1[i]===str[i])
+		{
+			console.log("true");
+		}
+		else {
+			console.log("false");
+		}
+	}
+	
 }
 module.exports = palindrome
